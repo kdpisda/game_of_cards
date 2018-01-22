d29001e670bf86d6326aef71cf822c4ff38c8cc5
feat7 <vinaykhobragade99@gmail.com>
2018-01-22 16:32:48 +0530

    Card class init

--- main.cpp
+++ main.cpp
@@ -2,7 +2,47 @@
 
 using namespace std;
 
+enum Rank (TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE);
+enum Suit (SPADE, HEART, DIAMOND, CLUB);
+
+class Card
+{
+	private:
+		int Suit, Rank;
+
+	public:
+		Card(int Suit, int Rank) {
+			this->Suit = Suit;
+			this->Rank = Rank;
+		}
+
+		//Getters
+
+		int GetRank() {
+			return this->Rank;
+		}
+
+		int GetSuit() {
+			return this->Suit;
+		}
+
+		//Setters
+
+		int SetRank(int Rank) {
+			this->Rank = Rank;
+			return this->Rank;
+		}
+
+		int SetSuit(int Suit) {
+			this->Suit = Suit;
+			return this->Suit;
+		}
+
+		~Card();
+		
+};
+
 int main(){
-    cout << "Hello Game";
+    
     return 0;
 }
