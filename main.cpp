15169a4aa1da7c9b612a1db79640c36a2a66871d
Kuldeep Pisda <pisdak79@gmail.com>
2018-01-22 16:35:15 +0530

    Added deckOfCards init

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+class DeckOfCards{
+    private:
+        Card playing_cards[52];
+    public:
+        void initialize();
+}
+
+void DeckOfCards:: initialize(){
+    // for()
+}
+
 int main(){
     cout << "Hello Game";
     return 0;
